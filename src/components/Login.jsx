9ebcf3e28369a95afc72cc93ce738627d47e2b60
Daniyal ali <daniyalali12568@gmail.com>
2023-11-09 03:03:41 +0500

    FrontednIntegrateBackend

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,10 +2,37 @@ import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom';
 import "./mix.css";
 
-
 const Login = () => {
     const [passShow, setPassShow] = useState(false);
-    console.log(passShow);
+    const [inpval, setInpVal] = useState({
+        email: "",
+        password: ""
+    })
+    console.log(inpval);
+    const setVal = (e) => {
+        const { name, value } = e.target
+        setInpVal(() => {
+            return {
+                ...inpval,
+                [name]: value
+            }
+        })
+    }
+    const loginUser = (e) => {
+        e.preventDefault();
+        const { email, password } = inpval;
+        console.log(inpval);
+        if (!email || !password) {
+            alert("please fill up the input filled");
+        } else if (!email.includes("@")) {
+            alert("Enter You valid email")
+        } else if (password.length < 8) {
+            alert("please must be 8 char")
+        }
+        else {
+            alert("Login sucessfully")
+        }
+    }
     return (
         <>
             <section>
@@ -18,20 +45,23 @@ const Login = () => {
                         <div className="form_input">
                             <label htmlFor="email">Email</label>
                             <div className="two">
-                                <input name="email" id="email" placeholder='Enter Your Email' />
+                                <input name="email" id="email" value={inpval.email} onChange={setVal} placeholder='Enter Your Email' />
                             </div>
                         </div>
+
                         <div className="form_input">
                             <label htmlFor="password">Password</label>
                             <div className="two">
-                                <input type={!passShow ? "Password" : "text"} name="password" id="password" placeholder='Enter Your password' />
+                                <input type={!passShow ? "Password" : "text"} name="password" id="password" value={inpval.password} onChange={setVal} placeholder='Enter Your password' />
                                 <div className="showpass" onClick={() => setPassShow(!passShow)}>
                                     {!passShow ? "show" : "Hide"}
                                 </div>
                             </div>
                         </div>
-                        <button className='btn'>Login</button>
-                        <p>Don't have an Account?<NavLink to="/register"> Sign Up</NavLink> </p>
+
+                        <button className='btn' onClick={loginUser}>Login</button>
+                        <p>Don't have an Account?<NavLink to="/register">Sign Up</NavLink> </p>
+                        
                     </form>
                 </div>
             </section>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,59 +1,103 @@
-import "./mix.css"
-import React, { useState } from 'react'
-import { NavLink } from "react-router-dom";
 
+import "./mix.css";
+import React, { useState } from 'react';
+import { NavLink } from "react-router-dom";
+import axios from 'axios'; // Import Axios
 
 const Register = () => {
     const [passShow, setPassShow] = useState(false);
     const [cpassShow, setCPassShow] = useState(false);
-  return (
+    const [inpval, setInpval] = useState({
+        fname: "",
+        email: "",
+        password: "",
+        cpassword: ""
+    });
+
+    const setVal = (e) => {
+        const { name, value } = e.target;
+        setInpval((prevInputValues) => ({
+            ...prevInputValues,
+            [name]: value
+        }));
+    };
+
+    const addUserdata = async (e) => {
+        e.preventDefault();
+        const { fname, email, password, cpassword } = inpval;
+
+        if (!fname || !email || !password || !cpassword) {
+            alert("Please fill up all input fields.");
+        } else if (!email.includes("@")) {
+            alert("Enter a valid email address.");
+        } else if (password.length < 8) {
+            alert("Password must be at least 8 characters.");
+        } else if (cpassword.length < 8) {
+            alert("Confirm password must be at least 8 characters.");
+        } else if (password !== cpassword) {
+            alert("Passwords do not match.");
+        } else {
+            try {
+                const response = await axios.post("http://localhost:8000/register", {
+                    fname,
+                    email,
+                    password,
+                    cpassword
+                });
+                console.log(response.data);
+            } catch (error) {
+                console.error("Error:", error);
+            }
+        }
+    };
+    return (
         <section>
-        <div className="form_data">
-            <div className="form_heading">
-                <h1>SignUp</h1>
-                <p style={{textAlign:"center"}}>Please Signup and create your Account.</p>
+            <div className="form_data">
+                <div className="form_heading">
+                    <h1>SignUp</h1>
+                    <p style={{ textAlign: "center" }}>Please Signup and create your Account.</p>
+                </div>
+                <form>
+
+                    <div className="form_input">
+                        <label htmlFor="text">Name</label>
+                        <div className="two">
+                            <input name="fname" id="fname" value={inpval.fname} onChange={setVal} placeholder='Enter Your Name' />
+                        </div>
+                    </div>
+
+                    <div className="form_input">
+                        <label htmlFor="email">Email</label>
+                        <div className="two">
+                            <input name="email" id="email" value={inpval.email} onChange={setVal} placeholder='Enter Your Email' />
+                        </div>
+                    </div>
+
+                    <div className="form_input">
+                        <label htmlFor="password">Password</label>
+                        <div className="two">
+                            <input type={!passShow ? "Password" : "text"} name="password" value={inpval.password} id="password" onChange={setVal} placeholder='Enter Your password' />
+                            <div className="showpass" onClick={() => setPassShow(!passShow)}>
+                                {!passShow ? "show" : "Hide"}
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="form_input">
+                        <label htmlFor="password">Confim Password</label>
+                        <div className="two">
+                            <input type={!cpassShow ? "password" : "text"} value={inpval.cpassword} name="cpassword" id="cpassword" onChange={setVal} placeholder='Enter Your Confim Password' />
+                            <div className="showpass" onClick={() => setCPassShow(!cpassShow)} >
+                                {!cpassShow ? "show" : "Hide"}
+                            </div>
+                        </div>
+                    </div>
+                    <button className='btn' onClick={addUserdata}>SignUP</button>
+                    <p>Don't have an Account? <NavLink to="/">LogIn</NavLink></p>
+                </form>
             </div>
-            <form>
-
-                <div className="form_input">
-                    <label htmlFor="text">Name</label>
-                    <div className="two">
-                        <input name="text" id="fname" placeholder='Enter Your Name' />
-                    </div>
-                </div>
-
-                <div className="form_input">
-                    <label htmlFor="email">Email</label>
-                    <div className="two">
-                        <input name="email" id="email" placeholder='Enter Your Email' />
-                    </div>
-                </div>
-               
-                <div className="form_input">
-                    <label htmlFor="password">Password</label>
-                    <div className="two">
-                        <input type={!passShow ? "Password" : "text"} name="password" id="password" placeholder='Enter Your password' />
-                        <div className="showpass" onClick={() => setPassShow(!passShow)}>
-                            {!passShow ? "show" : "Hide"}
-                        </div>
-                    </div>
-                </div>
-
-                <div className="form_input">
-                    <label htmlFor="password">Confim Password</label>
-                    <div className="two">
-                        <input  type={!cpassShow?"password":"text"} name="cpassword" id="cpassword" placeholder='Enter Your Confim Password' />
-                        <div className="showpass" onClick={()=> setCPassShow(!cpassShow)} >
-                            {!cpassShow ? "show" : "Hide"}
-                        </div>
-                    </div>
-                </div>
-                <button className='btn'>SignUP</button>
-                <p>Don't have an Account? <NavLink to="/">LogIn</NavLink></p>
-            </form>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Register
